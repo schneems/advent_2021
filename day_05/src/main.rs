47fead816b8ffa3e414ce36fa216cd0f46660ee7
schneems <richard.schneeman+foo@gmail.com>
2021-12-06 12:17:06 -0600

    Parse input

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Line {
+    start: (u32, u32),
+    end: (u32, u32),
+}
+
+impl Line {
+    fn from(line: &str) -> Self {
+        if let Some((Some(start), Some(end))) = line.split_once("->").map(|(start, end)| {
+            (
+                start
+                    .trim()
+                    .split_once(",")
+                    .map(|(x, y)| (x.parse().unwrap(), y.parse().unwrap())),
+                end.trim()
+                    .split_once(",")
+                    .map(|(x, y)| (x.parse().unwrap(), y.parse().unwrap())),
+            )
+        }) {
+            Line { start, end }
+        } else {
+            panic!("nope")
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lol() {
+        let line = Line::from("403,277 -> 403,802");
+        assert_eq!(line.start, (403, 277));
+        assert_eq!(line.end, (403, 802));
+    }
+}
