45964be784f220ab731e30c0c11c592bed691639
schneems <richard.schneeman+foo@gmail.com>
2021-12-16 11:47:29 -0600

    Update AOC script

--- aoc.rb
+++ aoc.rb
@@ -11,25 +11,43 @@ end
 def cargo_new(dir)
   run!("cargo new #{dir}")
   dir.join("src/main.rs").write(<<~'EOM')
-    fn main() {
-        let out = part_1(include_str!("../input.txt"));
-        println!("part_1: {}", out);
-    }
+// use std::collections::HashMap;
+// use std::str::FromStr;
 
-    fn part_1(input: &str) -> u64 {
-        unimplemented!()
-    }
+fn main() {
+    let out = part_1(include_str!("../input.txt"));
+    println!("part_1: {}", out);
 
-    #[cfg(test)]
-    mod tests {
-        use super::*;
+    let out = part_2(include_str!("../input.txt"));
+    println!("part_2: {}", out);
+}
 
-        #[test]
-        fn test_part_1() {
-          let input = r#""#;
-          assert_eq!(part_1(input), 99);
-        }
+fn part_1(input: &str) -> u64 {
+    let _thing = parse(input);
+    unimplemented!()
+}
+
+fn part_2(input: &str) -> u64 {
+    let _thing = parse(input);
+    unimplemented!()
+}
+
+fn parse(input: &str) {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parts() {
+        let input = r#""#;
+        assert_eq!(part_1(input), 99);
+        // assert_eq!(part_2(input), 99);
     }
+}
+
   EOM
 end
 
--- day_16/src/main.rs
+++ day_16/src/main.rs
@@ -1,9 +1,25 @@
+// use std::collections::HashMap;
+// use std::str::FromStr;
+
 fn main() {
     let out = part_1(include_str!("../input.txt"));
     println!("part_1: {}", out);
+
+    let out = part_2(include_str!("../input.txt"));
+    println!("part_2: {}", out);
 }
 
 fn part_1(input: &str) -> u64 {
+    let _thing = parse(input);
+    unimplemented!()
+}
+
+fn part_2(input: &str) -> u64 {
+    let _thing = parse(input);
+    unimplemented!()
+}
+
+fn parse(input: &str) {
     unimplemented!()
 }
 
@@ -12,8 +28,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_1() {
-      let input = r#""#;
-      assert_eq!(part_1(input), 99);
+    fn test_parts() {
+        let input = r#""#;
+        assert_eq!(part_1(input), 99);
+        // assert_eq!(part_2(input), 99);
     }
 }
