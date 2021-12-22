dd6aeb2701a4fdcb23ac8accb341d12859f38a23
schneems <richard.schneeman+foo@gmail.com>
2021-12-22 08:12:31 -0800

    Part 2

--- day_21/src/main.rs
+++ day_21/src/main.rs
@@ -28,11 +28,36 @@ fn deterministic_roll_three(at: &mut u64, dice: &mut u64) {
 }
 
 fn part_1(one: u64, two: u64) -> u64 {
-    play(one, two)
+    play_nice(one, two)
 }
 
-fn part_2(one: u64, two: u64) -> u64 {
-    unimplemented!()
+fn part_2(start_one: u64, start_two: u64) -> u64 {
+    let (one, two) = play_lol(start_one, 0, start_two, 0);
+    println!("{} {}", one, two);
+    std::cmp::max(one, two)
+}
+
+fn play_lol(one_at: u64, one_score: u64, two_at: u64, two_score: u64) -> (u64, u64) {
+    if two_score >= 21 {
+        return (0, 1);
+    }
+
+    if one_score >= 21 {
+        return (1, 0);
+    }
+
+    let mut one_count = 0;
+    let mut two_count = 0;
+    for (num, multiplier) in [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)] {
+        let new_one_at = 1 + (num + one_at - 1) % 10;
+        let new_one_score = one_score + new_one_at;
+
+        let (out_two, out_one) = play_lol(two_at, two_score, new_one_at, new_one_score);
+        one_count += out_one * multiplier;
+        two_count += out_two * multiplier;
+    }
+
+    (one_count, two_count)
 }
 
 fn num_to_score(num: u64) -> u64 {
@@ -44,7 +69,7 @@ fn deterministic_roll_three_player(player: &mut Player, dice: &mut u64) {
     player.score += player.at;
 }
 
-fn play(one: u64, two: u64) -> u64 {
+fn play_nice(one: u64, two: u64) -> u64 {
     let mut count = 0;
     let mut one = Player { at: one, score: 0 };
     let mut two = Player { at: two, score: 0 };
@@ -70,6 +95,26 @@ fn play(one: u64, two: u64) -> u64 {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_omfg() {
+        // let (one, two) = play_lol(1, 21, 1, 20);
+        // assert_eq!(one, 1);
+        // assert_eq!(two, 0);
+
+        // let (one, two) = play_lol(1, 20, 1, 21);
+        // assert_eq!(one, 0);
+        // assert_eq!(two, 1);
+
+        // let (one, two) = play_lol(1, 20, 4, 20);
+        // assert_eq!(one, 0);
+        // assert_eq!(two, 27);
+
+        println!("============");
+        let (one, two) = play_lol(4, 20, 1, 20);
+        assert_eq!(one, 27);
+        assert_eq!(two, 0);
+    }
 
     #[test]
     fn test_deterministic_roll_three() {
@@ -98,8 +143,7 @@ mod tests {
 
     #[test]
     fn test_parts() {
-        let input = r#""#;
         assert_eq!(part_1(4, 8), 739785);
-        // assert_eq!(part_2(input), 99);
+        assert_eq!(part_2(4, 8), 444356092776315);
     }
 }
