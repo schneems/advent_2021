291a745e213e89f347b2e435eb98251ccd4e4e4c
schneems <richard.schneeman+foo@gmail.com>
2021-12-26 16:27:53 -0600

    WIP

--- day_23/src/main.rs
+++ day_23/src/main.rs
@@ -1,5 +1,91 @@
-// use std::collections::HashMap;
+use std::collections::HashMap;
 // use std::str::FromStr;
+use std::cmp::Ordering;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+enum AmphipodColor {
+    A,
+    B,
+    C,
+    D,
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct Amphipod {
+    point: (i8, i8),
+    color: AmphipodColor,
+}
+
+struct Square {
+    room: Option<AmphipodColor>,
+}
+
+type Grid = HashMap<(i8, i8), Square>;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Position {
+    cost: u64,
+    ready: HashMap<(i8, i8), Amphipod>,
+    waiting: HashMap<(i8, i8), Amphipod>,
+    happy: HashMap<(i8, i8), Amphipod>,
+}
+
+impl Ord for Position {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.cost.cmp(&other.cost)
+    }
+}
+
+impl PartialOrd for Position {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn build_world_example() -> Grid {
+    let mut grid = Grid::new();
+    for i in 0..=10 {
+        grid.insert((0, i), Square { room: None });
+    }
+    for (i, j) in [(-1, 2), (-2, 2)] {
+        grid.insert(
+            (i, j),
+            Square {
+                room: Some(AmphipodColor::A),
+            },
+        );
+    }
+
+    for (i, j) in [(-1, 4), (-2, 4)] {
+        grid.insert(
+            (i, j),
+            Square {
+                room: Some(AmphipodColor::B),
+            },
+        );
+    }
+
+    for (i, j) in [(-1, 6), (-2, 6)] {
+        grid.insert(
+            (i, j),
+            Square {
+                room: Some(AmphipodColor::C),
+            },
+        );
+    }
+
+    for (i, j) in [(-1, 8), (-2, 8)] {
+        grid.insert(
+            (0, i),
+            Square {
+                room: Some(AmphipodColor::C),
+            },
+        );
+    }
+    grid
+}
 
 fn main() {
     let out = part_1(include_str!("../input.txt"));
@@ -19,8 +105,134 @@ fn part_2(input: &str) -> u64 {
     unimplemented!()
 }
 
-fn parse(input: &str) {
-    unimplemented!()
+fn parse(input: &str) -> Position {
+    let mut lines = input.trim().lines().into_iter();
+
+    lines.next();
+    lines.next();
+
+    let mut ready = HashMap::new();
+    for (i, line) in lines.enumerate() {
+        let i = i as i8 * -1 + -1;
+        for (j, c) in line.chars().enumerate() {
+            let j = j as i8 + -1;
+            let amphi = match c {
+                'A' => Amphipod {
+                    color: AmphipodColor::A,
+                    point: (i, j),
+                },
+                'B' => Amphipod {
+                    color: AmphipodColor::B,
+                    point: (i, j),
+                },
+                'C' => Amphipod {
+                    color: AmphipodColor::C,
+                    point: (i, j),
+                },
+                'D' => Amphipod {
+                    color: AmphipodColor::D,
+                    point: (i, j),
+                },
+                '#' | ' ' => continue,
+                _ => panic!("Nope {:?}", c),
+            };
+            ready.insert((i, j), amphi);
+        }
+    }
+    Position {
+        cost: 0,
+        ready: ready,
+        waiting: HashMap::new(),
+        happy: HashMap::new(),
+    }
+}
+
+fn color_to_str(color: &AmphipodColor) -> String {
+    match color {
+        &AmphipodColor::A => "A".to_string(),
+        &AmphipodColor::B => "B".to_string(),
+        &AmphipodColor::C => "C".to_string(),
+        &AmphipodColor::D => "D".to_string(),
+    }
+}
+
+fn print(grid: Grid, position: Position) {
+    println!("");
+    for i in (-2..=0).rev() {
+        let mut line = Vec::new();
+        for j in 0..10 {
+            if let Some(a) = position.ready.get(&(i, j)) {
+                line.push(color_to_str(&a.color));
+            } else if let Some(a) = position.waiting.get(&(i, j)) {
+                line.push(color_to_str(&a.color));
+            } else {
+                if let Some(s) = grid.get(&(i, j)) {
+                    line.push(".".to_string());
+                } else {
+                    line.push("#".to_string())
+                }
+            }
+        }
+        println!("{:?}", line.join(""));
+    }
+}
+
+fn play(grid: Grid, position: Position) -> u64 {
+    let mut frontier = BinaryHeap::new();
+    frontier.push(Reverse(position.clone()));
+
+    if let Some(Reverse(position)) = frontier.pop() {
+        if position.ready.is_empty() && position.waiting.is_empty() {
+            return position.cost;
+        }
+
+        for (point, amphi) in &position.ready {
+            let mut p = position.clone();
+            let amphi = p.ready.remove(point).unwrap();
+
+            if let Some(square) = grid.get(point) {
+                if square.room.as_ref().unwrap() != &amphi.color {
+                } else {
+                    let below = (point.0 - 1, point.1);
+                    match grid.get(&below) {
+                        None => {
+                            p.happy.insert(*point, amphi);
+                            frontier.push(Reverse(p));
+                        }
+                        Some(s) => {
+                            if let Some(a) = position.happy.get(&below) {
+                                p.happy.insert(*point, amphi);
+                                frontier.push(Reverse(p));
+                            } else if let Some(a) = position.ready.get(&below) {
+                                // Amphipod blocking, move into the hallway
+                                for j in [-1, 1] {
+                                    let mut next = p.clone();
+                                    next.ready.insert((0, point.0 + j), amphi.clone());
+                                    next.cost +=
+                                        dist(*point, (0, point.0 + j)) * cost_color(&amphi.color);
+                                    frontier.push(Reverse(next));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    0
+}
+
+fn cost_color(color: &AmphipodColor) -> u64 {
+    match color {
+        &AmphipodColor::A => 1,
+        &AmphipodColor::B => 10,
+        &AmphipodColor::C => 100,
+        &AmphipodColor::D => 1000,
+    }
+}
+
+fn dist(a: (i8, i8), b: (i8, i8)) -> u64 {
+    ((a.0 - b.0).abs() + (a.0 - b.0).abs()).try_into().unwrap()
 }
 
 #[cfg(test)]
@@ -28,10 +240,26 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_lol() {
+        let grid = build_world_example();
+        let position = parse(
+            r#"
+#############
+#...........#
+###B#C#B#D###
+  #A#D#C#A#
+  #########
+"#,
+        );
+
+        let cost = play(grid, position);
+        assert_eq!(cost, 12521);
+    }
+
+    #[test]
     fn test_parts() {
         let input = r#""#;
-        assert_eq!(part_1(input), 99);
+        // assert_eq!(part_1(input), 99);
         // assert_eq!(part_2(input), 99);
     }
 }
-
--- day_24/src/main.rs
+++ day_24/src/main.rs
@@ -1,5 +1,144 @@
 // use std::collections::HashMap;
-// use std::str::FromStr;
+use std::str::FromStr;
+
+#[derive(Hash, Clone, Debug, PartialEq, Eq)]
+struct ALU {
+    w: i16,
+    x: i16,
+    y: i16,
+    z: i16,
+}
+
+fn inp(val: &mut i16, by: i16) -> bool {
+    *val = by;
+    true
+}
+
+fn add(val: &mut i16, by: i16) -> bool {
+    *val += by;
+    true
+}
+
+fn mul(val: &mut i16, by: i16) -> bool {
+    *val *= by;
+    true
+}
+
+fn div(val: &mut i16, by: i16) -> bool {
+    if by == 0 {
+        false
+    } else {
+        *val = num::integer::div_floor(*val, by);
+        true
+    }
+}
+
+fn modit(val: &mut i16, by: i16) -> bool {
+    if val < &mut 0 || by <= 0 {
+        false
+    } else {
+        *val %= by;
+        true
+    }
+}
+
+fn eql(val: &mut i16, by: i16) -> bool {
+    if *val == by {
+        *val = 1;
+    } else {
+        *val = 0;
+    }
+    true
+}
+
+#[derive(Clone, Debug)]
+struct Command {
+    function: FFS,
+    val_char: Register,
+    by_char: Register,
+}
+
+#[derive(Clone, Debug)]
+enum FFS {
+    INP,
+    ADD,
+    MUL,
+    DIV,
+    MOD,
+    EQL,
+}
+
+fn ffs_from_str(string: &str) -> FFS {
+    match string {
+        "inp" => FFS::INP,
+        "add" => FFS::ADD,
+        "mul" => FFS::MUL,
+        "div" => FFS::DIV,
+        "mod" => FFS::MOD,
+        "eql" => FFS::EQL,
+        _ => panic!("nope {}", string),
+    }
+}
+
+#[derive(Clone, Debug)]
+enum Register {
+    W,
+    X,
+    Y,
+    Z,
+    INPUT,
+    VAL(i16),
+}
+
+fn register_from_str(string: &str) -> Register {
+    match string {
+        "w" => Register::W,
+        "x" => Register::X,
+        "y" => Register::Y,
+        "z" => Register::Z,
+        _ => Register::VAL(string.parse::<i16>().unwrap()),
+    }
+}
+
+fn parse(input: &str) -> Vec<Vec<Command>> {
+    let mut out = Vec::new();
+
+    let mut lines = input.trim().lines().into_iter();
+
+    let mut instructions: Vec<Command> = vec![];
+    while let Some(line) = lines.next() {
+        let mut parts = line.split(" ").into_iter();
+        match parts.next() {
+            Some(s) if s == "inp" => {
+                if &instructions.len() > &0 {
+                    out.push(instructions);
+                }
+
+                instructions = Vec::new();
+
+                instructions.push(Command {
+                    function: FFS::INP,
+                    val_char: register_from_str(parts.next().unwrap()),
+                    by_char: Register::INPUT,
+                })
+            }
+            Some(s) => {
+                //
+                instructions.push(Command {
+                    function: ffs_from_str(s),
+                    val_char: register_from_str(parts.next().unwrap()),
+                    by_char: register_from_str(parts.next().unwrap()),
+                })
+            }
+            None => panic!("WAT"),
+        }
+    }
+
+    if &instructions.len() > &0 {
+        out.push(instructions);
+    }
+    out
+}
 
 fn main() {
     let out = part_1(include_str!("../input.txt"));
@@ -10,17 +149,114 @@ fn main() {
 }
 
 fn part_1(input: &str) -> u64 {
-    let _thing = parse(input);
-    unimplemented!()
+    let instructions = parse(input);
+    let mut max = 0;
+    for x in (11111111111111..=39924989499970 as i64).rev() {
+        let mut alu = ALU {
+            w: 0,
+            x: 0,
+            y: 0,
+            z: 0,
+        };
+
+        run_commands(&mut alu, &instructions, x);
+
+        if alu.z == 0 {
+            max = x;
+            break;
+        }
+    }
+    max.try_into().unwrap()
 }
 
 fn part_2(input: &str) -> u64 {
-    let _thing = parse(input);
-    unimplemented!()
+    let instructions = parse(input);
+    let mut min = 0;
+    let mut count = 0;
+    for x in 16111111111111..=39924989499969 as i64 {
+        count += 1;
+        if count == 10_000_000 {
+            count = 0;
+            println!("{}", x);
+        }
+        let mut alu = ALU {
+            w: 0,
+            x: 0,
+            y: 0,
+            z: 0,
+        };
+
+        run_commands(&mut alu, &instructions, x);
+
+        if alu.z == 0 {
+            min = x;
+            break;
+        }
+    }
+    min.try_into().unwrap()
 }
 
-fn parse(input: &str) {
-    unimplemented!()
+fn run_commands(alu: &mut ALU, instructions: &Vec<Vec<Command>>, x: i64) {
+    let num_str = x.to_string();
+    let mut parts = num_str.chars();
+    if parts.clone().any(|x| x == '0') {
+        alu.z = 1;
+        return;
+    }
+
+    for instruction in instructions {
+        let in_str = parts.next().unwrap().to_string();
+        if run_mut(alu, &instruction, in_str.parse::<i16>().unwrap()) == false {
+            alu.z = 1;
+            break;
+        }
+    }
+}
+
+fn run_mut(alu: &mut ALU, commands: &Vec<Command>, input: i16) -> bool {
+    for command in commands {
+        let function = match command.function {
+            FFS::INP => inp,
+            FFS::MUL => mul,
+            FFS::ADD => add,
+            FFS::DIV => div,
+            FFS::MOD => modit,
+            FFS::EQL => eql,
+        };
+
+        let by = match command.by_char {
+            Register::W => alu.w.clone(),
+            Register::X => alu.x.clone(),
+            Register::Y => alu.y.clone(),
+            Register::Z => alu.z.clone(),
+            Register::INPUT => input,
+            Register::VAL(x) => x.clone(),
+        };
+
+        let val = match command.val_char {
+            Register::W => &mut alu.w,
+            Register::X => &mut alu.x,
+            Register::Y => &mut alu.y,
+            Register::Z => &mut alu.z,
+            _ => panic!("nope {:?}", command.val_char),
+        };
+
+        if function(val, by) == false {
+            return false;
+        }
+    }
+    true
+}
+
+fn run(commands: &Vec<Command>, input: i16) -> ALU {
+    let mut alu = ALU {
+        w: 0,
+        x: 0,
+        y: 0,
+        z: 0,
+    };
+    run_mut(&mut alu, commands, input);
+    alu
 }
 
 #[cfg(test)]
@@ -28,10 +264,53 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_parse() {
+        let out = parse(
+            r#"
+inp w
+add z w
+mod z 2
+div w 2
+add y w
+mod y 2
+div w 2
+add x w
+mod x 2
+div w 2
+mod w 2
+"#,
+        );
+        assert_eq!(
+            run(&out.get(0).unwrap().to_vec(), 9),
+            ALU {
+                w: 1,
+                x: 0,
+                y: 0,
+                z: 1,
+            }
+        );
+
+        let out = parse(
+            r#"
+inp w
+inp x
+inp y
+inp z
+"#,
+        );
+        assert_eq!(out.len(), 4);
+    }
+
+    #[test]
     fn test_parts() {
-        let input = r#""#;
-        assert_eq!(part_1(input), 99);
-        // assert_eq!(part_2(input), 99);
+        let commands = parse(include_str!("../input.txt"));
+        let mut alu = ALU {
+            w: 0,
+            x: 0,
+            y: 0,
+            z: 0,
+        };
+        run_commands(&mut alu, &commands, 39924989499969);
+        assert_eq!(alu.z, 0);
     }
 }
-
