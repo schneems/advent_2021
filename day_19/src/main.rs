2bf60c6be0ecb7c952c091295c9ba2c1a8a65b9a
schneems <richard.schneeman+foo@gmail.com>
2021-12-27 11:56:45 -0600

    Fix parsing

--- day_19/src/main.rs
+++ day_19/src/main.rs
@@ -1,4 +1,4 @@
-// use std::collections::HashMap;
+use std::collections::HashMap;
 // use std::str::FromStr;
 
 fn main() {
@@ -19,8 +19,76 @@ fn part_2(input: &str) -> u64 {
     unimplemented!()
 }
 
-fn parse(input: &str) {
-    unimplemented!()
+type Point = (i16, i16, i16);
+
+fn distance(a: Point, b: Point) -> i16 {
+    (a.0 - b.0).abs() + (a.1 - b.1).abs() + (a.2 - b.2).abs()
+}
+
+fn parse(input: &str) -> Vec<Scanner> {
+    let mut out = Vec::new();
+    let mut lines = input.trim().lines().into_iter();
+
+    let mut beacons = Vec::new();
+    while let Some(line) = lines.next() {
+        println!("{:?}", line);
+        let mut chars = line.chars().peekable();
+        if chars.peek().is_none() {
+            continue;
+        }
+        if chars.last() == Some('-') {
+            if beacons.len() > 0 {
+                out.push(Scanner::new(beacons));
+                beacons = Vec::new();
+            }
+            continue;
+        }
+
+        println!("Adding to beacon");
+        let mut nums = line.split(",").into_iter();
+        beacons.push((
+            nums.next().unwrap().parse().unwrap(),
+            nums.next().unwrap().parse().unwrap(),
+            nums.next().unwrap().parse().unwrap(),
+        ))
+    }
+    out.push(Scanner::new(beacons));
+
+    out
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Scanner {
+    beacons: Vec<Point>,
+    pairs: HashMap<i16, (Point, Point)>,
+}
+
+impl Scanner {
+    fn new(beacons: Vec<Point>) -> Self {
+        let mut pairs = HashMap::new();
+        for one in beacons.iter() {
+            for two in beacons.iter() {
+                if one != two {
+                    pairs.insert(distance(*one, *two), (*one, *two));
+                }
+            }
+        }
+        Scanner { beacons, pairs }
+    }
+
+    fn find_overlap_becaon(&self, other: &Self) -> (Point, Point) {
+        // let mut vec_one = Vec::new();
+        // let mut vec_two = Vec::new();
+        for (dist, one) in &self.pairs {
+            println!("{:?}", dist);
+            if let Some(two) = other.pairs.get(&dist) {
+                println!("======");
+                println!("{:?}", one);
+                println!("{:?}", two);
+            }
+        }
+        ((0, 0, 0), (0, 0, 0))
+    }
 }
 
 #[cfg(test)]
@@ -28,10 +96,54 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parts() {
-        let input = r#""#;
-        assert_eq!(part_1(input), 99);
-        // assert_eq!(part_2(input), 99);
+    fn test_match_points() {
+        let scanners = parse(
+            r#"
+--- scanner 0 ---
+0,2,0
+4,1,0
+3,3,0
+
+--- scanner 1 ---
+-1,-1,0
+-5,0,0
+-2,1,0
+"#,
+        );
+
+        let pair = scanners[0].find_overlap_becaon(&scanners[1]);
+        // assert_eq!(pair.0, (0, 2, 0));
+        // assert_eq!(pair.0, (-1, -1, 0));
+    }
+
+    #[test]
+    fn test_blerf() {
+        let scanners = parse(
+            r#"
+--- scanner 0 ---
+0,2,0
+4,1,0
+3,3,0
+
+--- scanner 1 ---
+-1,-1,0
+-5,0,0
+-2,1,0
+"#,
+        );
+
+        assert_eq!(distance((0, 2, 0), (4, 1, 0)), 5);
+        assert_eq!(scanners.len(), 2);
+        assert_eq!(scanners[0].pairs.get(&5).unwrap(), &((4, 1, 0), (0, 2, 0)));
+        assert_eq!(scanners[0].pairs.len(), 3);
+
+        assert_eq!(scanners[1].beacons.len(), 3);
+        assert_eq!(scanners[1].pairs.len(), 3);
+    }
+
+    #[test]
+    fn test_distance() {
+        assert_eq!(distance((1, 0, 0), (2, 0, 0)), 1);
+        assert_eq!(distance((2, 0, 0), (2, 0, 0)), 0);
     }
 }
-
