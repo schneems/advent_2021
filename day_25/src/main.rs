2aea5b96379853b4a9459c822a30a7e6f4800a9b
schneems <richard.schneeman+foo@gmail.com>
2021-12-27 09:33:11 -0600

    Parse

--- day_25/src/main.rs
+++ day_25/src/main.rs
@@ -1,4 +1,4 @@
-// use std::collections::HashMap;
+use std::collections::HashMap;
 // use std::str::FromStr;
 
 fn main() {
@@ -19,8 +19,35 @@ fn part_2(input: &str) -> u64 {
     unimplemented!()
 }
 
-fn parse(input: &str) {
-    unimplemented!()
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+enum Cucumber {
+    Down,
+    Right,
+    None,
+}
+
+type Grid = HashMap<(i16, i16), Cucumber>;
+
+fn parse(input: &str) -> Grid {
+    input
+        .trim()
+        .lines()
+        .enumerate()
+        .flat_map(|(i, line)| {
+            line.chars()
+                .enumerate()
+                .map(|(j, char)| {
+                    let cucumber = match char {
+                        '>' => Cucumber::Right,
+                        'v' => Cucumber::Down,
+                        '.' => Cucumber::None,
+                        _ => panic!("at the disco {}", char),
+                    };
+                    ((i as i16, j as i16), cucumber)
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<HashMap<_, _>>()
 }
 
 #[cfg(test)]
@@ -28,10 +55,14 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_equality() {
+        assert_eq!(parse("...>>>>>..."), parse("...>>>>>..."));
+    }
+
+    #[test]
     fn test_parts() {
         let input = r#""#;
-        assert_eq!(part_1(input), 99);
+        // assert_eq!(part_1(input), 99);
         // assert_eq!(part_2(input), 99);
     }
 }
-
