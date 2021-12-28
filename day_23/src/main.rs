41321379eea1bef9db75c0c8d4f10737f5111085
schneems <richard.schneeman+foo@gmail.com>
2021-12-28 15:22:53 -0600

    Restart

--- day_23/src/main.rs
+++ day_23/src/main.rs
@@ -1,91 +1,165 @@
-use std::collections::HashMap;
+// use std::collections::HashMap;
 // use std::str::FromStr;
-use std::cmp::Ordering;
+// use std::cmp::Ordering;
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 
-#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+type Point = (i8, i8);
+
+#[derive(Copy, Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
 enum AmphipodColor {
     A,
     B,
     C,
     D,
+    None,
 }
 
-#[derive(Debug, Clone, Hash, PartialEq, Eq)]
-struct Amphipod {
-    point: (i8, i8),
-    color: AmphipodColor,
+#[derive(Debug, Clone, PartialEq, Eq, Ord, PartialOrd)]
+struct Board {
+    last: AmphipodColor,
+    hallway: [AmphipodColor; 10],
+    a: [AmphipodColor; 2],
+    b: [AmphipodColor; 2],
+    c: [AmphipodColor; 2],
+    d: [AmphipodColor; 2],
 }
 
-struct Square {
-    room: Option<AmphipodColor>,
-}
-
-type Grid = HashMap<(i8, i8), Square>;
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct Position {
-    cost: u64,
-    ready: HashMap<(i8, i8), Amphipod>,
-    waiting: HashMap<(i8, i8), Amphipod>,
-    happy: HashMap<(i8, i8), Amphipod>,
-}
-
-impl Ord for Position {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.cost.cmp(&other.cost)
+impl Board {
+    fn color_is_happy(&self, color: &AmphipodColor) -> bool {
+        let room = match color {
+            &AmphipodColor::A => self.a,
+            &AmphipodColor::B => self.b,
+            &AmphipodColor::C => self.c,
+            &AmphipodColor::D => self.d,
+            &AmphipodColor::None => panic!("nope"),
+        };
+        room.iter().all(|c| c == color)
     }
 }
 
-impl PartialOrd for Position {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
+fn parse(input: &str) -> Board {
+    let mut lines = input.trim().lines().into_iter();
+    let mut hallway = [AmphipodColor::None; 10];
+    let mut a = [AmphipodColor::None; 2];
+    let mut b = [AmphipodColor::None; 2];
+    let mut c = [AmphipodColor::None; 2];
+    let mut d = [AmphipodColor::None; 2];
+
+    lines.next();
+    let hall_chars = lines.next().unwrap().chars().collect::<Vec<char>>();
+    for (i, c) in hall_chars[1..11].iter().enumerate() {
+        hallway[i] = match c {
+            'A' => AmphipodColor::A,
+            'B' => AmphipodColor::B,
+            'C' => AmphipodColor::C,
+            'D' => AmphipodColor::D,
+            '#' => continue,
+            '.' => AmphipodColor::None,
+            ' ' => continue,
+            _ => panic!("Highly unexpected {}", c),
+        };
+    }
+    let mut index = 0;
+    while let Some(line) = lines.next() {
+        let chars = line.chars().collect::<Vec<char>>();
+
+        for i in [3, 5, 7, 9] {
+            let color = match chars[i] {
+                'A' => AmphipodColor::A,
+                'B' => AmphipodColor::B,
+                'C' => AmphipodColor::C,
+                'D' => AmphipodColor::D,
+                '#' => continue,
+                '.' => AmphipodColor::None,
+                ' ' => continue,
+                _ => panic!("Highly unexpected {}", chars[i]),
+            };
+            match i {
+                3 => a[index] = color,
+                5 => b[index] = color,
+                7 => c[index] = color,
+                9 => d[index] = color,
+                _ => panic!("Oops {}", i),
+            }
+        }
+
+        index += 1;
+    }
+
+    let last = AmphipodColor::None;
+    Board {
+        last,
+        hallway,
+        a,
+        b,
+        c,
+        d,
     }
 }
 
-fn build_world_example() -> Grid {
-    let mut grid = Grid::new();
-    for i in 0..=10 {
-        grid.insert((0, i), Square { room: None });
-    }
-    for (i, j) in [(-1, 2), (-2, 2)] {
-        grid.insert(
-            (i, j),
-            Square {
-                room: Some(AmphipodColor::A),
-            },
-        );
-    }
+// #[derive(Debug, Clone, PartialEq, Eq)]
+// struct Position {
+//     cost: u64,
+//     ready: HashMap<(i8, i8), Amphipod>,
+//     waiting: HashMap<(i8, i8), Amphipod>,
+//     happy: HashMap<(i8, i8), Amphipod>,
+// }
 
-    for (i, j) in [(-1, 4), (-2, 4)] {
-        grid.insert(
-            (i, j),
-            Square {
-                room: Some(AmphipodColor::B),
-            },
-        );
-    }
+// impl Ord for Position {
+//     fn cmp(&self, other: &Self) -> Ordering {
+//         self.cost.cmp(&other.cost)
+//     }
+// }
 
-    for (i, j) in [(-1, 6), (-2, 6)] {
-        grid.insert(
-            (i, j),
-            Square {
-                room: Some(AmphipodColor::C),
-            },
-        );
-    }
+// impl PartialOrd for Position {
+//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+//         Some(self.cmp(other))
+//     }
+// }
 
-    for (i, j) in [(-1, 8), (-2, 8)] {
-        grid.insert(
-            (0, i),
-            Square {
-                room: Some(AmphipodColor::C),
-            },
-        );
-    }
-    grid
-}
+// fn build_world_example() -> Grid {
+//     let mut grid = Grid::new();
+//     for i in 0..=10 {
+//         grid.insert((0, i), Square { room: None });
+//     }
+//     for (i, j) in [(-1, 2), (-2, 2)] {
+//         grid.insert(
+//             (i, j),
+//             Square {
+//                 room: Some(AmphipodColor::A),
+//             },
+//         );
+//     }
+
+//     for (i, j) in [(-1, 4), (-2, 4)] {
+//         grid.insert(
+//             (i, j),
+//             Square {
+//                 room: Some(AmphipodColor::B),
+//             },
+//         );
+//     }
+
+//     for (i, j) in [(-1, 6), (-2, 6)] {
+//         grid.insert(
+//             (i, j),
+//             Square {
+//                 room: Some(AmphipodColor::C),
+//             },
+//         );
+//     }
+
+//     for (i, j) in [(-1, 8), (-2, 8)] {
+//         grid.insert(
+//             (0, i),
+//             Square {
+//                 room: Some(AmphipodColor::C),
+//             },
+//         );
+//     }
+//     grid
+// }
 
 fn main() {
     let out = part_1(include_str!("../input.txt"));
@@ -105,134 +179,54 @@ fn part_2(input: &str) -> u64 {
     unimplemented!()
 }
 
-fn parse(input: &str) -> Position {
-    let mut lines = input.trim().lines().into_iter();
-
-    lines.next();
-    lines.next();
-
-    let mut ready = HashMap::new();
-    for (i, line) in lines.enumerate() {
-        let i = i as i8 * -1 + -1;
-        for (j, c) in line.chars().enumerate() {
-            let j = j as i8 + -1;
-            let amphi = match c {
-                'A' => Amphipod {
-                    color: AmphipodColor::A,
-                    point: (i, j),
-                },
-                'B' => Amphipod {
-                    color: AmphipodColor::B,
-                    point: (i, j),
-                },
-                'C' => Amphipod {
-                    color: AmphipodColor::C,
-                    point: (i, j),
-                },
-                'D' => Amphipod {
-                    color: AmphipodColor::D,
-                    point: (i, j),
-                },
-                '#' | ' ' => continue,
-                _ => panic!("Nope {:?}", c),
-            };
-            ready.insert((i, j), amphi);
-        }
-    }
-    Position {
-        cost: 0,
-        ready: ready,
-        waiting: HashMap::new(),
-        happy: HashMap::new(),
-    }
-}
-
 fn color_to_str(color: &AmphipodColor) -> String {
     match color {
         &AmphipodColor::A => "A".to_string(),
         &AmphipodColor::B => "B".to_string(),
         &AmphipodColor::C => "C".to_string(),
         &AmphipodColor::D => "D".to_string(),
+        &AmphipodColor::None => ".".to_string(),
     }
 }
 
-fn print(grid: Grid, position: Position) {
-    println!("");
-    for i in (-2..=0).rev() {
-        let mut line = Vec::new();
-        for j in 0..10 {
-            if let Some(a) = position.ready.get(&(i, j)) {
-                line.push(color_to_str(&a.color));
-            } else if let Some(a) = position.waiting.get(&(i, j)) {
-                line.push(color_to_str(&a.color));
-            } else {
-                if let Some(s) = grid.get(&(i, j)) {
-                    line.push(".".to_string());
-                } else {
-                    line.push("#".to_string())
-                }
-            }
-        }
-        println!("{:?}", line.join(""));
+fn print(board: &Board) {
+    println!();
+    println!("###########");
+    println!(
+        "{}",
+        board
+            .hallway
+            .iter()
+            .map(color_to_str)
+            .collect::<Vec<_>>()
+            .join("")
+    );
+    for i in 0..2 {
+        print!("##{}", color_to_str(&board.a[i]));
+        print!("#{}", color_to_str(&board.b[i]));
+        print!("#{}", color_to_str(&board.c[i]));
+        print!("#{}", color_to_str(&board.d[i]));
+        println!("##");
     }
+    println!("###########");
 }
 
-fn play(grid: Grid, position: Position) -> u64 {
+fn play(board: Board) -> u64 {
     let mut frontier = BinaryHeap::new();
-    frontier.push(Reverse(position.clone()));
+    frontier.push(Reverse((0, 0, board.clone())));
 
-    if let Some(Reverse(position)) = frontier.pop() {
-        if position.ready.is_empty() && position.waiting.is_empty() {
-            return position.cost;
-        }
-
-        for (point, amphi) in &position.ready {
-            let mut p = position.clone();
-            let amphi = p.ready.remove(point).unwrap();
-
-            if let Some(square) = grid.get(point) {
-                if square.room.as_ref().unwrap() != &amphi.color {
-                } else {
-                    let below = (point.0 - 1, point.1);
-                    match grid.get(&below) {
-                        None => {
-                            p.happy.insert(*point, amphi);
-                            frontier.push(Reverse(p));
-                        }
-                        Some(s) => {
-                            if let Some(a) = position.happy.get(&below) {
-                                p.happy.insert(*point, amphi);
-                                frontier.push(Reverse(p));
-                            } else if let Some(a) = position.ready.get(&below) {
-                                // Amphipod blocking, move into the hallway
-                                for j in [-1, 1] {
-                                    let mut next = p.clone();
-                                    next.ready.insert((0, point.0 + j), amphi.clone());
-                                    next.cost +=
-                                        dist(*point, (0, point.0 + j)) * cost_color(&amphi.color);
-                                    frontier.push(Reverse(next));
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+    if let Some(Reverse(board)) = frontier.pop() {}
     0
 }
 
 fn cost_color(color: &AmphipodColor) -> u64 {
     match color {
-        &AmphipodColor::A => 1,
-        &AmphipodColor::B => 10,
-        &AmphipodColor::C => 100,
-        &AmphipodColor::D => 1000,
+        AmphipodColor::A => 1,
+        AmphipodColor::B => 10,
+        AmphipodColor::C => 100,
+        AmphipodColor::D => 1000,
+        AmphipodColor::None => panic!("Cannot move nothing"),
     }
-}
-
-fn dist(a: (i8, i8), b: (i8, i8)) -> u64 {
-    ((a.0 - b.0).abs() + (a.0 - b.0).abs()).try_into().unwrap()
 }
 
 #[cfg(test)]
@@ -240,9 +234,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_lol() {
-        let grid = build_world_example();
-        let position = parse(
+    fn test_endgame() {
+        let board = parse(
+            r#"
+#############
+#.........A.#
+###.#B#C#D###
+  #A#B#C#D#
+  #########
+"#,
+        );
+
+        assert_eq!(play(board), 8);
+    }
+
+    #[test]
+    fn test_parsing() {
+        let board = parse(
             r#"
 #############
 #...........#
@@ -252,8 +260,13 @@ mod tests {
 "#,
         );
 
-        let cost = play(grid, position);
-        assert_eq!(cost, 12521);
+        print(&board);
+        assert_eq!(&board.hallway, &[AmphipodColor::None; 10]);
+        assert_eq!(&board.a, &[AmphipodColor::B, AmphipodColor::A]);
+        assert_eq!(&board.b, &[AmphipodColor::C, AmphipodColor::D]);
+        assert_eq!(&board.c, &[AmphipodColor::B, AmphipodColor::C]);
+        assert_eq!(&board.d, &[AmphipodColor::D, AmphipodColor::A]);
+        // panic!("lol");
     }
 
     #[test]
