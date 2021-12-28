a2d86aaacf93291cab8e1853363d2f5de1d695e8
schneems <richard.schneeman+foo@gmail.com>
2021-12-28 13:44:13 -0600

    Part 2 day 19 done

--- day_19/src/main.rs
+++ day_19/src/main.rs
@@ -17,14 +17,26 @@ fn part_1(input: &str) -> u64 {
 }
 
 fn part_2(input: &str) -> u64 {
-    let _thing = parse(input);
-    unimplemented!()
+    let scanners = fix_scanners(&parse(input));
+    let mut max_dist = 0;
+    for i in 0..scanners.len() {
+        for j in 0..scanners.len() {
+            let dist = distance(
+                &scanners[i].position.unwrap(),
+                &scanners[j].position.unwrap(),
+            );
+            if dist > max_dist {
+                max_dist = dist;
+            }
+        }
+    }
+    max_dist.try_into().unwrap()
 }
 
 type Point = (i16, i16, i16);
 
-fn distance(a: Point, b: Point) -> i16 {
-    (a.0 - b.0).abs() + (a.1 - b.1).abs() + (a.2 - b.2).abs()
+fn distance(a: &Point, b: &Point) -> i64 {
+    (a.0 - b.0).abs() as i64 + (a.1 - b.1).abs() as i64 + (a.2 - b.2).abs() as i64
 }
 
 fn parse(input: &str) -> Vec<Scanner> {
@@ -66,7 +78,7 @@ struct Scanner {
     num: i16,
     position: Option<Point>,
     beacons: Vec<Point>,
-    pairs: HashMap<i16, (Point, Point)>,
+    pairs: HashMap<i64, (Point, Point)>,
 }
 
 impl Scanner {
@@ -75,7 +87,7 @@ impl Scanner {
         for one in beacons.iter() {
             for two in beacons.iter() {
                 if one != two {
-                    pairs.insert(distance(*one, *two), (*one, *two));
+                    pairs.insert(distance(one, two), (*one, *two));
                 }
             }
         }
@@ -156,11 +168,11 @@ impl Scanner {
 
         let mut new_beacons = self.beacons.clone();
         rotate_flip_translate_vec(&mut new_beacons, rotation, flip, translation);
-        let scanner = Scanner::new(new_beacons, self.num);
 
+        self.position = Some(translation);
+        let scanner = Scanner::new(new_beacons, self.num);
         self.beacons = scanner.beacons;
         self.pairs = scanner.pairs;
-        self.position = Some(translation);
     }
 }
 
@@ -300,6 +312,7 @@ fn fix_scanners(scanners: &Vec<Scanner>) -> Vec<Scanner> {
             found = true;
 
             unknown.fix_orientation(known);
+            break;
         }
 
         if found {
@@ -316,6 +329,11 @@ fn fix_scanners(scanners: &Vec<Scanner>) -> Vec<Scanner> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_part2_given() {
+        assert_eq!(part_2(include_str!("../given.txt")), 3621);
+    }
 
     #[test]
     fn blerg() {
@@ -561,7 +579,7 @@ mod tests {
 "#,
         );
 
-        assert_eq!(distance((0, 2, 0), (4, 1, 0)), 5);
+        assert_eq!(distance(&(0, 2, 0), &(4, 1, 0)), 5);
         assert_eq!(scanners.len(), 2);
         assert_eq!(scanners[0].pairs.get(&5).unwrap(), &((4, 1, 0), (0, 2, 0)));
         assert_eq!(scanners[0].pairs.len(), 3);
@@ -572,150 +590,11 @@ mod tests {
 
     #[test]
     fn test_distance() {
-        assert_eq!(distance((1, 0, 0), (2, 0, 0)), 1);
-        assert_eq!(distance((2, 0, 0), (2, 0, 0)), 0);
+        assert_eq!(distance(&(1, 0, 0), &(2, 0, 0)), 1);
+        assert_eq!(distance(&(2, 0, 0), &(2, 0, 0)), 0);
     }
 
     fn given_case() -> Vec<Scanner> {
-        parse(
-            r#"
---- scanner 0 ---
-404,-588,-901
-528,-643,409
--838,591,734
-390,-675,-793
--537,-823,-458
--485,-357,347
--345,-311,381
--661,-816,-575
--876,649,763
--618,-824,-621
-553,345,-567
-474,580,667
--447,-329,318
--584,868,-557
-544,-627,-890
-564,392,-477
-455,729,728
--892,524,684
--689,845,-530
-423,-701,434
-7,-33,-71
-630,319,-379
-443,580,662
--789,900,-551
-459,-707,401
-
---- scanner 1 ---
-686,422,578
-605,423,415
-515,917,-361
--336,658,858
-95,138,22
--476,619,847
--340,-569,-846
-567,-361,727
--460,603,-452
-669,-402,600
-729,430,532
--500,-761,534
--322,571,750
--466,-666,-811
--429,-592,574
--355,545,-477
-703,-491,-529
--328,-685,520
-413,935,-424
--391,539,-444
-586,-435,557
--364,-763,-893
-807,-499,-711
-755,-354,-619
-553,889,-390
-
---- scanner 2 ---
-649,640,665
-682,-795,504
--784,533,-524
--644,584,-595
--588,-843,648
--30,6,44
--674,560,763
-500,723,-460
-609,671,-379
--555,-800,653
--675,-892,-343
-697,-426,-610
-578,704,681
-493,664,-388
--671,-858,530
--667,343,800
-571,-461,-707
--138,-166,112
--889,563,-600
-646,-828,498
-640,759,510
--630,509,768
--681,-892,-333
-673,-379,-804
--742,-814,-386
-577,-820,562
-
---- scanner 3 ---
--589,542,597
-605,-692,669
--500,565,-823
--660,373,557
--458,-679,-417
--488,449,543
--626,468,-788
-338,-750,-386
-528,-832,-391
-562,-778,733
--938,-730,414
-543,643,-506
--524,371,-870
-407,773,750
--104,29,83
-378,-903,-323
--778,-728,485
-426,699,580
--438,-605,-362
--469,-447,-387
-509,732,623
-647,635,-688
--868,-804,481
-614,-800,639
-595,780,-596
-
---- scanner 4 ---
-727,592,562
--293,-554,779
-441,611,-461
--714,465,-776
--743,427,-804
--660,-479,-426
-832,-632,460
-927,-485,-438
-408,393,-506
-466,436,-512
-110,16,151
--258,-428,682
--393,719,612
--211,-452,876
-808,-476,-593
--575,615,604
--485,667,467
--680,325,-822
--627,-443,-432
-872,-547,-609
-833,512,582
-807,604,487
-839,-516,451
-891,-625,532
--652,-548,-490
-30,-46,-14
-"#,
-        )
+        parse(include_str!("../given.txt"))
     }
 }
